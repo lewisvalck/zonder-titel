912c6317c1af9cf99b482ad8cbaabbf8477fbbc7
lewisvalck <135333053+lewisvalck@users.noreply.github.com>
2023-12-01 12:21:20 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,1 +1,61 @@
-
+let ir_waarde = 0
+let vooruit = 1
+let achteruit = 2
+let links = 3
+let rechts = 4
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    ir_waarde += IR.IR_read()
+})
+basic.forever(function () {
+    if (ir_waarde == vooruit) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == achteruit) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == links) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == rechts) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    let stop = 0
+    if (ir_waarde == stop) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
